25381cc20fafffd1c703bc9faa623966a7afa23f
nwinden <noah.winden@gmail.com>
2016-04-28 16:59:02 -0500

    initial commit

--- group_01.js
+++ group_01.js
@@ -1,22 +1,40 @@
-var atticus = ["Atticus", "2405", "47000", 3];
-var jem = ["Jem", "62347", "63500", 4];
-var boo = ["Boo", "11435", "54000", 3];
-var scout = ["Scout", "6243", "74750", 5];
+var atticus = new Person("Atticus", "2405", "47000", 3);
+var jem = new Person ("Jem", "62347", "63500", 4);
+var boo = new Person("Boo", "11435", "54000", 3);
+var scout = new Person("Scout", "6243", "74750", 5);
 
 
 var employees = [atticus, jem, boo, scout];
 
-//function that accesses the individual employee information to derive the total compensation they should recieve based on thier bonus.
-//@param employee : the array of employees that we get our data from to find the bonus.
+/**
+*creates a person object to represent an employee at STI
+*@param name: name of the employee
+*@param employeeNumber: the employee number
+*@param salary: the employees salary
+*@param rating: the current employee rating
+**/
+
+function Person (name, employeeNumber, salary, rating) {
+  this.name = name,
+  this.employeeNumber= employeeNumber,
+  this.salary=salary,
+  this.rating=rating
+}
+
+/**
+*function that accesses the individual employee information to derive the total compensation they should recieve based on thier bonus.
+* @param employee : the array of employees that we get our data from to find the bonus.
+**/
+
 function findBonus(employee) {
 
   var array = [];
-  var rating = employee[3];
+  var rating = employee.rating;
   var bonusPercent = 0;
-  var salary = parseInt(employee[2]);
+  var sal = parseInt(employee.salary);
   var bonusAmount = 0;
 
-  array[0] = employee[0];
+  array[0] = employee.name;
 
   switch (rating) {
     case 3:
@@ -33,11 +51,11 @@ function findBonus(employee) {
       break;
     }
 
-    if(employee[1].length == 4){
+    if(employee.employeeNumber.length == 4){
       bonusPercent += .05;
     }
 
-    if(employee[2] > 65000){
+    if(sal > 65000){
       bonusPercent -= .01;
     }
 
@@ -47,16 +65,19 @@ function findBonus(employee) {
 
     array[1] = bonusPercent;
 
-    bonusAmount = salary * bonusPercent;
+    bonusAmount = sal * bonusPercent;
 
-    array[2] = salary + bonusAmount;
+    array[2] = sal + bonusAmount;
 
     array[3] = Math.round(bonusAmount);
 
     return array;
 }
 
-// loops through an array of employees and prints out the relevant bonus information for each employee.
+/**
+*loops through an array of employees and prints out the relevant bonus information for each employee on a website.
+**/
+
 for (var i = 0; i<employees.length; i++){
 
   var face = findBonus(employees[i]);
